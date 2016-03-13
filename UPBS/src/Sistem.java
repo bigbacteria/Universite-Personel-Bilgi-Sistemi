3215c18f6c959de3dd9d60720d60f8341d595618
Abdullah AKALIN <abdullah.akalin@gazi.edu.tr>
2016-03-13 18:09:59 -0400

    Sistem.java Dosyası Oluşturuldu Bu dosyanın -main- metodunu içermesi planlanıyor.

---
+++ UPBS/src/Sistem.java
@@ -0,0 +1,17 @@
+package UPBS;
+
+import java.awt.*;
+
+public class Sistem {
+   
+   public static void main( String[] args ) {
+      EventQueue.invokeLater( new Runnable()
+      {
+         @Override
+         public void run() {
+            AnaPencere p = new AnaPencere();
+            p.setVisible( true );
+         }
+      } );
+   }
+}
